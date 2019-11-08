bb1bd2801457c6d05c850f4517f8e9896021566d
Jovan Karuna Cahyadi <52835257+JovanKaruna@users.noreply.github.com>
2019-11-08 15:34:40 +0700

    Add files via upload

---
+++ IF2121_K03_G11_1.pl
@@ -0,0 +1,92 @@
+/* Definisi Fakta */
+  /* Nama Tokemon */
+  tokemon(alphamon).
+  tokemon(betamon).
+  tokemon(cupmon).
+  tokemon(donutmon).
+  tokemon(eclairmon).
+  tokemon(froyomon).
+  tokemon(gingermon).
+  tokemon(honeymon).
+  tokemon(icemon).
+  tokemon(jellymon).
+  tokemon(kitmon).
+  tokemon(lollimon).
+  tokemon(marshmon).
+  tokemon(nougatmon).
+  tokemon(oreomon).
+  tokemon(piemon).
+
+  /* Jenis Tokemon */
+  normal(alphamon).
+  legendary(betamon).
+  normal(cupmon).
+  normal(donutmon).
+  normal(eclairmon).
+  normal(froyomon).
+  normal(gingermon).
+  normal(honeymon).
+  legendary(icemon).
+  normal(jellymon).
+  normal(kitmon).
+  normal(lollimon).
+  normal(marshmon).
+  normal(nougatmon).
+  legendary(oreomon).
+  normal(piemon).
+
+  /* Health Tokemon */
+  health(alphamon,50).
+  health(betamon,350).
+  health(cupmon,70).
+  health(donutmon,60).
+  health(eclairmon,90).
+  health(froyomon,75).
+  health(gingermon,40).
+  health(honeymon,80).
+  health(icemon,420).
+  health(jellymon,10).
+  health(kitmon,100).
+  health(lollimon,95).
+  health(marshmon,25).
+  health(nougatmon,110).
+  health(oreomon,1000).
+  health(piemon,90).
+  
+  /* Tipe Tokemon */
+  tipe(alphamon,fire).
+  tipe(betamon,leaves).
+  tipe(cupmon,water).
+  tipe(donutmon,lightning).
+  tipe(eclairmon,fire).
+  tipe(froyomon,leaves).
+  tipe(gingermon,water).
+  tipe(honeymon,lightning).
+  tipe(icemon,fire).
+  tipe(jellymon,leaves).
+  tipe(kitmon,water).
+  tipe(lollimon,lightning).
+  tipe(marshmon,water).
+  tipe(nougatmon,leaves).
+  tipe(oreomon,fire).
+  tipe(piemon,lightning).
+  
+  /* Normal Attack Tokemon */
+  damage(alphamon,8).
+  damage(betamon,40).
+  damage(cupmon,2).
+  damage(donutmon,10).
+  damage(eclairmon,4).
+  damage(froyomon,15).
+  damage(gingermon,12).
+  damage(honeymon,6).
+  damage(icemon,60).
+  damage(jellymon,10).
+  damage(kitmon,3).
+  damage(lollimon,10).
+  damage(marshmon,25).
+  damage(nougatmon,6).
+  damage(oreomon,100).
+  damage(piemon,8).
+  
+  
